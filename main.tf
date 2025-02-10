d2699a5f0d1e00813612eecfc29c5bc1994d29f8
yamuna <malini.mahathi@gmail.com>
2025-02-10 16:09:07 +0530

    module

--- main.tf
+++ main.tf
@@ -109,17 +109,17 @@ resource "aws_route" "database" {
   
 }
 resource "aws_route_table_association" "public_route" {
-  count=lenghth(var.public_subnet_cidr)
+  count=length(var.public_subnet_cidr)
   subnet_id      = aws_subnet.public[count.index].id
   route_table_id = aws_route_table.public.id
 }
 resource "aws_route_table_association" "private_route" {
-  count=lenghth(var.public_subnet_cidr)
+  count=length(var.public_subnet_cidr)
   subnet_id      = aws_subnet.private[count.index].id
   route_table_id = aws_route_table.private.id
 }
 resource "aws_route_table_association" "database_route" {
-  count=lenghth(var.database_subnet_cidr)
+  count=length(var.database_subnet_cidr)
   subnet_id      = aws_subnet.database[count.index].id
   route_table_id = aws_route_table.database.id
 }
