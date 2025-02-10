c55e3301a81808cd7866b1ae61bc6d7b037ac1fc
yamuna <malini.mahathi@gmail.com>
2025-02-10 17:37:07 +0530

    module

--- peering.tf
+++ peering.tf
@@ -1,5 +1,5 @@
 resource "aws_vpc_peering_connection" "peering" {
- count=var.is_peering_required ? 0 : 1
+ count=var.is_peering_required ? 1: 0
   peer_vpc_id   = local.default_vpc_id #requester
   vpc_id        = aws_vpc.main.id #accepter
   auto_accept   = true
@@ -11,25 +11,25 @@ resource "aws_vpc_peering_connection" "peering" {
 
 }
 resource "aws_route" "public-peering" {
-    count =var.is_peering_required ?0 :1
+    count =var.is_peering_required ?1:0
   route_table_id            = aws_route_table.public.id
   destination_cidr_block    = local.default_vpc_cidr
   vpc_peering_connection_id = aws_vpc_peering_connection.peering[count.index].id
 }
 resource "aws_route" "private-peering" {
-    count =var.is_peering_required ?0 :1
-  route_table_id            = aws_route_table.private.id
+    count =var.is_peering_required ?1 :0
+      route_table_id            = aws_route_table.private.id
   destination_cidr_block    = local.default_vpc_cidr
   vpc_peering_connection_id = aws_vpc_peering_connection.peering[count.index].id
 }
 resource "aws_route" "database-peering" {
-    count =var.is_peering_required ?0 :1
+    count =var.is_peering_required ?1 :0
   route_table_id            = aws_route_table.database.id
   destination_cidr_block    = local.default_vpc_cidr
   vpc_peering_connection_id = aws_vpc_peering_connection.peering[count.index].id
 }
 resource "aws_route" "default-peering" {
-    count =var.is_peering_required ?0 :1
+    count =var.is_peering_required ? 1:0
   route_table_id            = data.aws_route_table.default.route_table_id
   destination_cidr_block    =var.cidr_block
   vpc_peering_connection_id = aws_vpc_peering_connection.peering[count.index].id
