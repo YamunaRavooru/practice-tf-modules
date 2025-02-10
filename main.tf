08de0821fdde95e2f4963fb476b73da79135a25b
yamuna <malini.mahathi@gmail.com>
2025-02-10 13:41:23 +0530

    module

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "aws_vpc" "main" {
   cidr_block       = var.cidr_block
-  instance_tenancy = default
+  instance_tenancy ="default"
   enable_dns_support =var.enable_dns_support
   tags =merge(var.common_tags, 
   {
